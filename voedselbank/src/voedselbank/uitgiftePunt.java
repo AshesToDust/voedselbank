afcee582c08d911d8eac92bd3fe6bb9fffd4a4ea
ebisschops <hi@eddybisschops.nl>
2017-05-29 15:25:00 +0200

    Update

--- voedselbank/src/voedselbank/uitgiftePunt.java
+++ voedselbank/src/voedselbank/uitgiftePunt.java
@@ -1,30 +1,19 @@
 package voedselbank;
+
 import java.awt.Dimension;
 import java.sql.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.table.*;
 
 public class uitgiftePunt {
 
+    // Vars
     private static Connection connection;
     private static Statement statement;
     private static ResultSet rs;
     
-    /**
-     * Deze methode zorgt er voor dat op basis van de (meta)data in de
-     * meegegeven resultset een object wordt voorbereid die door JTable
-     * kan worden gebruikt om de data in een grid te tonen.
-     * 
-     * @param rs - De resultset waaruit de data wordt uitgelezen.
-     * @return - Het object met data en metadata dat door JTable wordt
-     *      gebruikt om de data gestructureerd te tonen op het scherm.
-     * @throws SQLException - Let op: Hier wordt data gelezen en dat kan fout gaan.
-     *      Op de plek waar deze methode wordt gebruikt moet de SQLException
-     *      worden afgehandeld.
-     */
+    // Table formatter
     public static DefaultTableModel fillTable (ResultSet rs) throws SQLException {
         
         // De metadata wordt uit de resultset opgehaald.
@@ -54,7 +43,7 @@ public class uitgiftePunt {
             Vector<Object> vector = new Vector<Object>();
             vector.add(rs.getObject ("locatie"));
             vector.add(rs.getObject ("maxPakketten"));
-            vector.add("#");
+            vector.add("0%");
             data.add (vector);
         }
 
@@ -65,9 +54,7 @@ public class uitgiftePunt {
         return new DefaultTableModel (data, columnNames);
     }
     
-    /**
-     * @param args the command line arguments
-     */
+    // Main
     public static void main(String[] args) {
         
         // Windows feel
@@ -75,38 +62,39 @@ public class uitgiftePunt {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         } 
         catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-           //
         }
 
-        //
-        
+        // Swing vars
         JFrame frame = new JFrame();
-        frame.setSize(1000, 500);
-        frame.setTitle("VOEHAA - Uitgiftepunten");
         JPanel panel = new JPanel();
-
+        
+        // Database
         try {
+            
+            // Data uit database halen
             connection = SimpleDataSourceV2.getConnection();
             statement = connection.createStatement();
-
-            // TO DO: Voeg hier je eigen query toe.
-            String query = "SELECT locatie, maxPakketten FROM UitgiftePunt;";
-
-            rs = statement.executeQuery (query);
+            rs = statement.executeQuery ("SELECT locatie, maxPakketten FROM UitgiftePunt;");
             
+            // Data in een JTable zetten om later te gebruiken
             JTable table = new JTable (fillTable (rs));
-            JScrollPane scrollPane = new JScrollPane (table);
-            scrollPane.setPreferredSize(new Dimension(970, 440));
-            
+            JScrollPane scrollPane = new JScrollPane(table);
+            scrollPane.setPreferredSize(new Dimension(400, 550));
             panel.add(scrollPane);
+            
         }
-        // Zowel direct in de try als in de methode fillTable (throws SQLException)
-        // kan een SQLException ontstaan die moet worden afgehandeld.
         catch (SQLException ex) {
+            
+            // Er ging iets fout, melding weergeven
             System.out.println ("Er is iets mis met de database.");
-            // Logger.getLogger(Blok4WC2.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
-        frame.add (panel);
+        
+        // Swing - frame
+        frame.setSize(440, 640);
+        frame.setTitle("VOEHAA - Uitgiftepunten");
+        frame.add(panel);
+        
         // Het frame mag niet direct worden afgesloten als de gebruiker op het
         // rode kruisje klikt. Voordat het scherm wordt gesloten (en daarmee de
         // applicatie) moeten eerst de SQL-objecten en connectie met de database
@@ -119,9 +107,13 @@ public class uitgiftePunt {
                 SimpleDataSourceV2.closeConnection ();
                 System.out.println ("De database is succesvol afgesloten");
                 System.exit(0);
+                
             }
+            
         });
 
         frame.setVisible (true);
+        
     }
+    
 }
